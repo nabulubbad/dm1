eff1a7e2ab8d4f6a915080b8933d646d29395612
nabulubbad <nabulubbad@gmail.com>
2022-12-24 15:29:50 +0300

    Update and rename mmcSD to mmcSD.sh

--- mmcSD.sh
+++ mmcSD.sh
@@ -8,3 +8,11 @@ echo   "  /media/sdcard auto auto,nofail 0 0" >>fsfix
 tr --delete '\n' < fsfix > newf
 cat newf >>/etc/fstab
 rm fsfix newf
+# install emc
+apt update && apt install enigma2-plugin-extensions-enhancedmoviecenter -y
+echo "config.EMC.autoaudio=true">>/etc/enigma2/settings
+echo "config.EMC.autoaudio_ac3=true">>/etc/enigma2/settings
+echo "config.EMC.epglang=ar_AE">>/etc/enigma2/settings
+echo "config.EMC.audlang=en">>/etc/enigma2/settings
+
+
